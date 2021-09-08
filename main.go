19c33cb6a7034fe451e41e521d9b4e6dc9c0a401
Ravshan Yuldashev <38344113+rorua@users.noreply.github.com>
2021-09-08 17:18:27 +0500

    ravshan changes

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	sellRate := 0.1470
 	amount := 100.0
 	result := amount / sellRate
 	println(result)
+
+	// этот коммент был написан Равшаном
+	// я бы использовал fmt
+	fmt.Println(result)
+
 }
